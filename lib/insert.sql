7682f3e6f7e0651cffbebf00918c2cc31c294596
Patrick McCombs <pa.mccombs1@gmail.com>
2019-02-09 03:25:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,11 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
 
 INSERT INTO bears VALUES (1, "Mr. Chocolate", 10, "M", "brown", "savage", true)
-INSERT INTO bears VALUES (2, "Mr. Chocolate", 10, "M", "brown", "savage", true)
-INSERT INTO bears VALUES (3, "Mr. Chocolate", 10, "M", "brown", "savage", true)
-INSERT INTO bears VALUES (4, "Mr. Chocolate", 10, "M", "brown", "savage", true)
-INSERT INTO bears VALUES (5, "Mr. Chocolate", 10, "M", "brown", "savage", true)
-INSERT INTO bears VALUES (6, "Mr. Chocolate", 10, "M", "brown", "savage", true)
-INSERT INTO bears VALUES (7, "Mr. Chocolate", 10, "M", "brown", "savage", true)
-INSERT INTO bears VALUES (8, "Mr. Chocolate", 10, "M", "brown", "savage", true)
+INSERT INTO bears VALUES (2, "Rowdy", 3, "M", "red", "gentle", false)
+INSERT INTO bears VALUES (3, "Tabitha", 5, "F", "grey", "docile", true)
+INSERT INTO bears VALUES (4, "Sergeant Brown", 15, "M", "green", "stoic", true)
+INSERT INTO bears VALUES (5, "Melissa", 10, "F", "brown", "savage", true)
+INSERT INTO bears VALUES (6, "Grinch", 10, "M", "brown", "savage", true)
+INSERT INTO bears VALUES (7, "Wendy", 10, "M", "brown", "savage", true)
+INSERT INTO bears VALUES (8, "Reese", 10, "M", "brown", "savage", true)
 
