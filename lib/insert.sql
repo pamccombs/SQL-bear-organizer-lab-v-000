ad5f2a3331e843560957ae9f6f37c9a26121805a
Patrick McCombs <pa.mccombs1@gmail.com>
2019-02-09 03:34:34 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears VALUES (1, "Mr. Chocolate", 10, "M", "brown", "savage", 1);
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 10, "M", "brown", "savage", true);
 INSERT INTO bears VALUES (2, "Rowdy", 3, "M", "red", "gentle", 0);
 INSERT INTO bears VALUES (3, "Tabitha", 5, "F", "grey", "docile", 1);
 INSERT INTO bears VALUES (4, "Sergeant Brown", 15, "M", "green", "stoic", 1);
