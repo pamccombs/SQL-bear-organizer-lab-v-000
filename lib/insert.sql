4c2f9df9d7d53c89cc411091d2a95d4bc486fc63
Patrick McCombs <pa.mccombs1@gmail.com>
2019-02-09 03:21:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,11 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
 
 INSERT INTO bears VALUES (1, "Mr. Chocolate", 10, "M", "brown", "savage", true)
+INSERT INTO bears VALUES (2, "Mr. Chocolate", 10, "M", "brown", "savage", true)
+INSERT INTO bears VALUES (3, "Mr. Chocolate", 10, "M", "brown", "savage", true)
+INSERT INTO bears VALUES (4, "Mr. Chocolate", 10, "M", "brown", "savage", true)
+INSERT INTO bears VALUES (5, "Mr. Chocolate", 10, "M", "brown", "savage", true)
+INSERT INTO bears VALUES (6, "Mr. Chocolate", 10, "M", "brown", "savage", true)
+INSERT INTO bears VALUES (7, "Mr. Chocolate", 10, "M", "brown", "savage", true)
+INSERT INTO bears VALUES (8, "Mr. Chocolate", 10, "M", "brown", "savage", true)
+
